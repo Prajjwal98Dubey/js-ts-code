6144e2a9be85752fbb73ffe0d1f13773490994bc
Prajjwal98Dubey <prajjwaldubey003@gmail.com>
2024-07-15 23:55:00 +0530

    new commit

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,16 +1,9 @@
 // Refactor this code
 
 function getURL(dev = false, tasks = {}) {
-    let url = dev
-        ? `/tasks?status=ACTIVE&dev=true&size=20`
-        : '/tasks';
-
-    if (tasks.nextTasks) {
-        url += '?hasNext=true';
-    }
-
-    if (tasks.prevTasks) {
-        url = '/tasks?hasPrev=true';
-    }
-    return { url };
+    if (tasks.prevTask) return '/tasks?hasPrev=true';
+    let url = '/tasks'
+    url += dev ? `?status=ACTIVE&dev=true&size=20` : ""
+    if (tasks.nextTask) return { url: url + '?hasNext=true' }
+    return { url }
 }
